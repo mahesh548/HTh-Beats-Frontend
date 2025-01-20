0c7288d35b81e3055eda60c6d9e6300d1a43fada
mahesh548 <mkmjnp5@gmail.com>
2025-01-20 23:15:19 +0530

    feat: implement playlist loading logic; add PageLoader and refactor component structure for improved user experience

--- src/pages/Playlist.jsx
+++ src/pages/Playlist.jsx
@@ -1,6 +1,19 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import PageLoader from "./components/PageLoader";
+import utils from "../../utils";
 
-export default function playlists() {
+export default function playlist() {
   const { id } = useParams();
-  return <h1>Playlist id: {id}</h1>;
+  const [loader, setLoader] = useState(true);
+  useEffect(() => {
+    const getPlaylist = async () => {
+      const response = await utils.API("/playlist");
+    };
+  });
+  const CreatePlaylist = () => {
+    return <h1>hello</h1>;
+  };
+
+  return loader ? <PageLoader /> : <CreatePlaylist />;
 }
