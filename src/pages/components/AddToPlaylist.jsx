dbd3c809e5dc9d157a7ee4a1239c998381d7dda8
mahesh548 <mkmjnp5@gmail.com>
2025-02-03 18:09:31 +0530

    feat: enhance AddToPlaylist component; add Create playlist and Save changes buttons, and improve layout with CSS grid

--- src/pages/components/AddToPlaylist.jsx
+++ src/pages/components/AddToPlaylist.jsx
@@ -14,21 +14,32 @@ export default function AddToPlaylist({ songdIds, playlistIds }) {
   };
   return (
     <div className="floatingPage">
-      <div className="navbarFloating">
-        <BackButton />
-        <p>Add to playlists</p>
-      </div>
-      <div>
-        {user.users_playlists.map((item) => {
-          return (
-            <button className="plCont" onClick={() => toggleList(item.id)}>
-              <p>{item.title}</p>
-              <img
-                src={addedTo.includes(item.id) ? likeFilled : likeOutlined}
-              />
-            </button>
-          );
-        })}
+      <div className="addToCont">
+        <div className="navbarAddTo">
+          <BackButton />
+          <p>Add to playlists</p>
+        </div>
+        <div>
+          {user.users_playlists.map((item) => {
+            return (
+              <button className="plCont" onClick={() => toggleList(item.id)}>
+                <p>{item.title}</p>
+                <img
+                  src={addedTo.includes(item.id) ? likeFilled : likeOutlined}
+                />
+              </button>
+            );
+          })}
+        </div>
+        <div>
+          <button className="addToBut buttonSecondary">Create playlist</button>
+          <button
+            className="addToBut buttonPrimary"
+            style={{ backgroundColor: "wheat" }}
+          >
+            Save changes
+          </button>
+        </div>
       </div>
     </div>
   );
