ef86512400ec29c215fff9aed2daceb5f696a7a2
mahesh548 <mkmjnp5@gmail.com>
2025-02-05 14:47:23 +0530

    feat: integrate react-easy-sort for sortable list in QueuePage; enhance song reordering functionality

--- src/pages/components/QueuePage.jsx
+++ src/pages/components/QueuePage.jsx
@@ -1,9 +1,19 @@
-import { ReactSortable } from "react-sortablejs";
 import BackButton from "./BackButton";
 import { SortRounded } from "@mui/icons-material";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { songContext } from "./Song";
+import SortableList, { SortableItem } from "react-easy-sort";
+import { arrayMoveImmutable } from "array-move";
+
 export default function QueuePage() {
   const [chips, setChips] = useState("next");
+  const { Queue, setQueue } = useContext(songContext);
+
+  const onSortEnd = (oldIndex, newIndex) => {
+    /* setItems((array) => arrayMoveImmutable(array, oldIndex, newIndex)); */
+    console.log(oldIndex);
+    console.log(newIndex);
+  };
   return (
     <div className="floatingPage">
       <div className="navbarAddTo">
@@ -27,7 +37,21 @@ export default function QueuePage() {
           Previous
         </button>
       </div>
-      {chips == "next" && <div className="upNextCont"></div>}
+      {chips == "next" && (
+        <div className="upNextCont">
+          <SortableList
+            onSortEnd={onSortEnd}
+            className="list"
+            draggedItemClassName="dragged"
+          >
+            {Queue.playlist.list.map((item) => (
+              <SortableItem key={item.id}>
+                <div className="item">{item.title}</div>
+              </SortableItem>
+            ))}
+          </SortableList>
+        </div>
+      )}
     </div>
   );
 }
