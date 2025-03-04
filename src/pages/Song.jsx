1e79d9538762b9415d2ffdc917f8be9d45a38ea2
mahesh548 <mkmjnp5@gmail.com>
2025-03-04 17:16:33 +0530

    feat: implement Song component with API integration and loading state

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -1,3 +1,36 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import PageLoader from "./components/PageLoader";
+import utils from "../../utils";
+import { AuthContext } from "./components/Auth";
+import CreateSong from "./components/CreateSong";
+
 export default function Song() {
-  return;
+  const auth = useContext(AuthContext);
+  const { id } = useParams();
+  const [playlistData, setplaylistData] = useState(false);
+
+  const refineResponse = (response) => {
+    return response;
+  };
+
+  useEffect(() => {
+    setplaylistData(false);
+    const getSong = async () => {
+      const response = await utils.API(`/song?id=${id}`, "GET");
+
+      if (response.hasOwnProperty("more_info")) {
+        setplaylistData(refineResponse(response));
+      }
+    };
+    if (auth.user?.verified) {
+      getSong();
+    }
+  }, [auth.user, id]);
+
+  return playlistData == false ? (
+    <PageLoader />
+  ) : (
+    <CreateSong response={playlistData} />
+  );
 }
---
+++ src/pages/components/CreateSong.jsx
@@ -0,0 +1,3 @@
+export default function CreateSong(response) {
+  third;
+}
