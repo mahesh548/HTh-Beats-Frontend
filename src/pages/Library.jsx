f6205efdb3b388d35818f7af5405506f9412103b
mahesh548 <mkmjnp5@gmail.com>
2025-03-11 19:23:20 +0530

    feat: implement Library component with data fetching and loading state

--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,7 +1,29 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router";
+import PageLoader from "./components/PageLoader";
+import utils from "../../utils";
+import { AuthContext } from "./components/Auth";
 export default function Library() {
-  return (
-    <div className="page hiddenScrollbar" style={{ overflowY: "scroll" }}>
-      <p className="labelText">Library</p>
-    </div>
-  );
+  const auth = useContext(AuthContext);
+  const { id } = useParams();
+  const [LibraryData, setLibraryData] = useState(false);
+
+  const refineResponse = (response) => {
+    return response;
+  };
+
+  useEffect(() => {
+    setLibraryData(false);
+    const getLibraryData = async () => {
+      const response = await utils.BACKEND("/save", "GET");
+      if (response.hasOwnProperty("data") && response.data.length > 0) {
+        setLibraryData(refineResponse(response));
+      }
+    };
+    if (auth.user?.verified) {
+      getLibraryData();
+    }
+  }, [auth.user, id]);
+
+  return LibraryData == false ? <PageLoader /> : <></>;
 }
