1c28ad35ffcbaf75345f856ae91e9a1d63bb2028
Mahesh <mkmjnp5@gmail.com>
2025-03-14 22:02:28 +0530

    feat: implement ChipSort component with active/inactive button styles

--- src/pages/components/ChipSort.jsx
+++ src/pages/components/ChipSort.jsx
@@ -1,3 +1,14 @@
+import { useState } from "react";
+
 export default function ChipSort({ filterData, filter }) {
-  return <></>;
+  const [active, setActive] = useState({ parent: "all", value: false });
+  return (
+    <div className="ps-1 mt-2">
+      <div className="chipActive">
+        <button className="px-3 py-1 ">Playlist</button>
+        <button className="px-3 py-1 ">By HTh-Beats</button>
+      </div>
+      <div className="inActiveChip ms-2"></div>
+    </div>
+  );
 }
