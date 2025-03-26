e1dc81ff5c3565ae90938d975abb3cbd2693526d
Mahesh <mkmjnp5@gmail.com>
2025-03-26 13:41:31 +0530

    feat: enhance CreateSearch component; add search input handling with debounce and clear functionality

--- src/pages/components/CreateSearch.jsx
+++ src/pages/components/CreateSearch.jsx
@@ -1,4 +1,4 @@
-import { useContext, useMemo } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { AuthContext } from "./Auth";
 
 import searchSvgOutlined from "../../assets/icons/searchSvgOutlined.svg";
@@ -9,11 +9,20 @@ import { ArrowBack, Close } from "@mui/icons-material";
 export default function CreateSearch() {
   const auth = useContext(AuthContext);
   const { openElements, open, close } = useContext(HashContext);
+  const [searchInput, setSearchInput] = useState("");
 
   const discover = localStorage?.homeCache
     ? JSON.parse(localStorage.homeCache).radio
     : false;
 
+  let searchTimeOut = useRef(null);
+  useEffect(() => {
+    if (searchInput.length == 0) return;
+    clearTimeout(searchTimeOut.current);
+    searchTimeOut.current = setTimeout(() => {
+      console.log(`making request for: ${searchInput}`);
+    }, 1000);
+  }, [searchInput]);
   return (
     <div className="page hiddenScrollbar" style={{ overflowY: "scroll" }}>
       <div className="libraryNavCont px-2 position-relative">
@@ -53,10 +62,15 @@ export default function CreateSearch() {
             type="text"
             placeholder="Hey, what do you want to listen ?"
             className="srchInput"
+            value={searchInput}
+            onInput={(e) => setSearchInput(e.target.value)}
+            spellCheck={false}
           />
-          <button className="iconButton">
-            <Close />
-          </button>
+          {searchInput.length > 0 && (
+            <button className="iconButton" onClick={() => setSearchInput("")}>
+              <Close />
+            </button>
+          )}
         </div>
       </div>
     </div>
