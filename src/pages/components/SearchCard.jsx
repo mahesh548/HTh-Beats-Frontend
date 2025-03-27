de7f932b4947d14479d99f0bf4317dbbec76a5a4
Mahesh <mkmjnp5@gmail.com>
2025-03-27 16:21:09 +0530

    feat: enhance SearchCard component; add navigation functionality for search results

--- src/pages/components/SearchCard.jsx
+++ src/pages/components/SearchCard.jsx
@@ -1,5 +1,10 @@
+import { useNavigate } from "react-router";
 import utils from "../../../utils";
 export default function SearchCard({ data, ac }) {
+  const navigate = useNavigate();
+  const showResult = (type, id) => {
+    navigate(`/${type}/${id}`);
+  };
   return (
     <div className="playlistSong mt-4">
       <img
@@ -8,8 +13,12 @@ export default function SearchCard({ data, ac }) {
         className={`playlistSongImg rounded ${
           data.type == "artist" ? "rounded-circle" : ""
         }`}
+        onClick={() => showResult(data.type, data?.perma_url || data?.url)}
       />
-      <div className={`${ac ? "extendedGrid" : ""}`}>
+      <div
+        className={`${ac ? "extendedGrid" : ""}`}
+        onClick={() => showResult(data.type, data?.perma_url || data?.url)}
+      >
         <p
           className="thinOneLineText playlistSongTitle"
           style={{ color: false ? "wheat" : "#ffffff" }}
