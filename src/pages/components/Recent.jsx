36c415d731ef87609914a5263a18f19e824da4f2
Mahesh <mkmjnp5@gmail.com>
2025-04-05 23:32:42 +0530

    feat: enhance Recent component; sort recent tracks by updated date and improve layout with CSS grid

--- src/pages/components/Recent.jsx
+++ src/pages/components/Recent.jsx
@@ -1,23 +1,24 @@
 import utils from "../../../utils";
 
 export default function Recent({ recentData }) {
-  console.log("recentData", recentData);
-  recentData = recentData.filter((item) => item.data);
-  recentData = recentData.map((item) => item.data);
+  recentData = recentData
+    .filter((item) => item.data)
+    .sort((a, b) => {
+      return new Date(b.updatedAt) - new Date(a.updatedAt);
+    })
+    .map((item) => item.data);
   const data =
     recentData.length % 2 == 0
       ? recentData
       : recentData.slice(0, recentData.length - 1);
 
-  console.log("recentData", recentData);
-
   return (
-    <div className="songArtCont">
+    <div className="recentCont mt-4">
       {data.map((item, index) => {
         return (
           <div key={`recent_${index}`} className="recentChip">
             <img src={item.image} />
-            <p className="thinTwoLineText">
+            <p className="thinTwoLineText mt-0">
               {utils.refineText(item?.name || item?.title)}
             </p>
           </div>
