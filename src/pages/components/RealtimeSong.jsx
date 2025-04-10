c2ae7eec133e0737401bc532bb17729c223fdcb1
Mahesh <mkmjnp5@gmail.com>
2025-04-11 00:04:00 +0530

    feat: enhance song playback handling in RealtimeSong component; fetch song details and update queue state

--- src/pages/components/RealtimeSong.jsx
+++ src/pages/components/RealtimeSong.jsx
@@ -1,17 +1,37 @@
 import { useContext, useEffect } from "react";
 import { channelContext } from "./Channel";
 import { songContext } from "./Song";
+import utils from "../../../utils";
 
 export default function RealtimeSong() {
   const { currentSong, setCurrentSong, roomInfo, playState, setPlayState } =
     useContext(channelContext);
   const { Queue, setQueue } = useContext(songContext);
 
+  const playThisSong = async (songId) => {
+    const response = await utils.API(`/song?id=${songId}`, "GET");
+
+    if (response.hasOwnProperty("more_info")) {
+      if (!Queue?.playlist) {
+        const playlistEcho = {
+          id: response.id,
+          title: roomInfo?.title,
+          type: "song",
+          list: [response],
+        };
+        setQueue({
+          type: "NEW",
+          value: { playlist: playlistEcho, song: response.id, status: "play" },
+        });
+      }
+    }
+  };
+
   useEffect(() => {
     //if remote user played the song
     if (!currentSong || !roomInfo) return;
     if (currentSong?.clientId == roomInfo?.clientId) return;
-
+    playThisSong(currentSong?.songId);
     console.log("song played by: ", currentSong.clientId);
   }, [currentSong?.songId]);
 
@@ -19,8 +39,11 @@ export default function RealtimeSong() {
     //if user played the song
     if (!roomInfo) return;
     if (!currentSong) {
+      const songId = Queue.playlist.list.find(
+        (item) => item.id == Queue.song
+      ).perma_url;
       setCurrentSong({
-        songId: Queue?.song,
+        songId: songId,
         clientId: roomInfo?.clientId,
       });
       return;
@@ -30,8 +53,12 @@ export default function RealtimeSong() {
       currentSong?.songId == Queue?.song
     )
       return;
+
+    const songId = Queue.playlist.list.find(
+      (item) => item.id == Queue.song
+    ).perma_url;
     setCurrentSong({
-      songId: Queue?.song,
+      songId: songId,
       clientId: roomInfo?.clientId,
     });
     console.log("you played song :", Queue?.song);
