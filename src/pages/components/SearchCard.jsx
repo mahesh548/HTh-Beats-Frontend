87d5a3cec5b3903bed788b0b3d5bbb59e003fc70
Mahesh <mkmjnp5@gmail.com>
2025-06-02 12:06:39 +0530

    feat: add error handling for image loading and update navigation logic in components

--- src/pages/components/SearchCard.jsx
+++ src/pages/components/SearchCard.jsx
@@ -22,7 +22,7 @@ export default function SearchCard({ data, ac, setGlobalLike }) {
       return;
     }
 
-    navigate(`/${data.type}/${data?.url || data?.id}`);
+    navigate(`/${data.type}/${data?.url || data?.perma_url}`);
   };
 
   //detecting if song is playable or not
